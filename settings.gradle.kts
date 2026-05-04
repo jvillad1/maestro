69e341ab0c3716dca4847ab5a1d5d8f70d9c007c
Juan Camilo Villada Ramirez <jvillad1@gmail.com>
2026-05-04 13:39:17 -0500

    fix: add nodejs.org/dist ivy repo for Wasm browser dev run

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,5 +14,13 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        ivy {
+            url = uri("https://nodejs.org/dist/")
+            patternLayout {
+                artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]")
+            }
+            metadataSources { artifact() }
+            content { includeModule("org.nodejs", "node") }
+        }
     }
 }
